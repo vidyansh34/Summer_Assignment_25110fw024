af3b2de8a76d53b3c3f0ce39ba94c220e8687295
vidyansh34 <vidyansh34@gmail.com>
2026-06-04 22:51:49 +0530

    Add files via upload

---
+++ Day-1/Q4.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n, count = 0;
+
+    cout << "Enter a number: ";
+    cin >> n;
+
+    while(n != 0) {
+        n = n / 10;
+        count++;
+    }
+
+    cout << "Number of digits = " << count;
+
+    return 0;
+}
