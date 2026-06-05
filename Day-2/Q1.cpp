f1c11b2e612c0d6fd4fd59022bc96f4eadff886b
vidyansh34 <vidyansh34@gmail.com>
2026-06-05 08:23:41 +0530

    Implement digit sum calculation in Q1.cpp
    Calculate the sum of the digits of a user-input number.

--- Day-2/Q1.cpp
+++ Day-2/Q1.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+int main() {
+    int sum = 0;
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    for(; num != 0; num = num / 10) {
+        sum = sum + (num % 10);
+        
+    }
+
+    cout << "Sum of digits = " << sum;
+
+    return 0;
+}
