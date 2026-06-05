7f5c69f4cf2e55a9f2ed8df32bb079c75e3a6244
vidyansh34 <vidyansh34@gmail.com>
2026-06-05 12:16:22 +0530

    Update Q1.cpp

--- Day-3/Q1.cpp
+++ Day-3/Q1.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+using namespace std;
+int main (){
 
+
+    int n;
+    cout<< " enter the number to check: "<< endl;
+    cin>>n;
+    bool prime =true;
+    if(n<=1){
+        prime =false;
+    }
+    else{
+        for(int i=2;i*i<=n;i++){
+            if(n%i==0){
+                prime=false;
+                break;
+            }
+        }
+    }
+    if(prime){
+        cout<< n << "is prime number";
+    }
+    else{
+        cout << n<< "is not prime number";
+    }
+    return 0;
+}
